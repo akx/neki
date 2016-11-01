7009adaf215e2120c33a4e0e2ea3bfb4d881f140
Aarni Koskela <akx@iki.fi>
2016-11-01 19:23:20 +0200

    Toolbarify

--- src/App.js
+++ src/App.js
@@ -3,29 +3,23 @@ import {Box, Flex} from 'reflexbox';
 import './App.css';
 import render from './render';
 import Button from 'react-bootstrap/lib/Button';
+import ButtonGroup from 'react-bootstrap/lib/ButtonGroup';
 import {connect} from 'react-redux';
 import {addSection, deleteSection, setSectionData, setSectionCollapse} from './actions/sections';
 
 const SectionEditor = connect()(({dispatch, section}) => {
     const changeData = (e) => dispatch(setSectionData(section, e.target.value));
-    const changeCollapse = (e) => dispatch(setSectionCollapse(section, !!e.target.checked));
     return (
         <Box p={1} mb={1} style={{background: 'silver'}}>
             <textarea value={section.data} onInput={changeData} onChange={changeData} />
-            <Flex>
-                <Box flexAuto>
-                    <Button bsSize="xsmall" bsStyle="danger" onClick={() => dispatch(deleteSection(section))}>Delete</Button>
-                    &nbsp;
-                    <Button bsSize="xsmall" onClick={() => dispatch(addSection(section, 'before'))}>&uarr; Add Above</Button>
-                    &nbsp;
-                    <Button bsSize="xsmall" onClick={() => dispatch(addSection(section, 'after'))}>&darr; Add Below</Button>
-                    &nbsp;
-                    <label>
-                        <input type="checkbox" checked={!!section.collapse} onChange={changeCollapse} />
-                        &nbsp;Layout next to last section
-                    </label>
-                </Box>
-            </Flex>
+            <ButtonGroup>
+                <Button bsSize="xsmall" bsStyle="danger" onClick={() => dispatch(deleteSection(section))}>Delete</Button>
+                <Button bsSize="xsmall" onClick={() => dispatch(addSection(section, 'before'))}>&uarr; Add Above</Button>
+                <Button bsSize="xsmall" onClick={() => dispatch(addSection(section, 'after'))}>&darr; Add Below</Button>
+                <Button bsSize="xsmall" onClick={() => dispatch(setSectionCollapse(section, !section.collapse))} selected={section.collapse}>
+                    Layout next to last section
+                </Button>
+            </ButtonGroup>
         </Box>
     );
 });
